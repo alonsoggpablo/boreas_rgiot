bb26e972d2aa688c28ea9455d27fafad53bde69e
Pablo <pablo@example.com>
2026-01-27 10:21:37 +0100

    Add airflow db init to reload script

--- reload_airflow.sh
+++ reload_airflow.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Reload Airflow webserver and scheduler using both docker-compose files
 
+
+echo "Initializing Airflow metadata database..."
+docker compose -f docker-compose.airflow.yml run --rm airflow-webserver airflow db init
+
 echo "Restarting Airflow webserver and scheduler..."
 docker compose -f docker-compose.yml -f docker-compose.airflow.yml restart airflow-webserver airflow-scheduler
 
